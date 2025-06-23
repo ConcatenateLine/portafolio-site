f77d63483fcbeb5f1a2cd05a38e4249ee714a63b
Concatenate Line <concatenateline@gmail.com>
2025-06-23 10:51:37 -0600

    Add trailing slash configuration to Astro site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site:  "https://concatenateline.github.io/portafolio-site/",
+  trailingSlash: 'always',
 });
